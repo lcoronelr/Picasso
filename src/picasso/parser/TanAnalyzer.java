7576f536c5bb3a93d6e161405e4309945e108be2
Therese Elvira Mombou Gatsing <tmombougatsing@mail.wlu.edu>
2025-11-17 19:51:09 -0500

    Implemented TanAnalyzer

--- src/picasso/parser/TanAnalyzer.java
+++ src/picasso/parser/TanAnalyzer.java
@@ -1,5 +1,22 @@
 package picasso.parser;
 
-public class TanAnalyzer {
+import java.util.Stack;
+
+import picasso.parser.language.ExpressionTreeNode;
+import picasso.parser.language.expressions.Tangent;
+import picasso.parser.tokens.Token;
+
+
+public class TanAnalyzer extends UnaryFunctionAnalyzer {
+	
+	@Override
+	public ExpressionTreeNode generateExpressionTree(Stack<Token> tokens) {
+		tokens.pop(); // Need to remove the floor token
+		// the parameter is the next token(s) on the stack.
+		// But, it needs to be processed
+		ExpressionTreeNode paramETN = SemanticAnalyzer.getInstance().generateExpressionTree(
+				tokens);
+		return new Tangent(paramETN);
+	}
 
 }
