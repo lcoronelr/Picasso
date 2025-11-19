9a021831ac1cbe3a3e44de4ac537582a82fc47c1
Abhishek Pradhan <apradhan@mail.wlu.edu>
2025-11-19 12:31:46 -0500

    second try pushing code

--- src/picasso/parser/rgbToYCrCbAnalyzer.java
+++ src/picasso/parser/rgbToYCrCbAnalyzer.java
@@ -1,9 +1,26 @@
 package picasso.parser;
 
-public class rgbToYCrCbAnalyzer {
+import java.util.Stack;
 
-	public rgbToYCrCbAnalyzer() {
-		// TODO Auto-generated constructor stub
+import picasso.parser.language.ExpressionTreeNode;
+import picasso.parser.language.expressions.Floor;
+import picasso.parser.tokens.Token;
+
+/** 
+ * Handles parsing the rgbToYCrCb function
+ * 
+ * @author Abhishek Pradhan
+ *
+ */
+public class rgbToYCrCbAnalyzer extends UnaryFunctionAnalyzer{
+	
+	@Override
+	
+	public ExpressionTreeNode generateExpressionTree(Stack<Token> tokens){
+		tokens.pop();
+		ExpressionTreeNode paramETN = SemanticAnalyzer.getInstance().generateExpressionTree(
+				tokens);
+		return new rgbToYCrCb(paramETN);
 	}
 
 }
