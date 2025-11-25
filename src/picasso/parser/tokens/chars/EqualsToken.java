28ebe8e157b7b76275893a9feeaadca9076222a7
Therese Elvira Mombou Gatsing <tmombougatsing@mail.wlu.edu>
2025-11-25 09:26:56 -0500

    Created EqualsToken and its constructor

---
+++ src/picasso/parser/tokens/chars/EqualsToken.java
@@ -0,0 +1,20 @@
+package picasso.parser.tokens.chars;
+
+import picasso.parser.language.CharConstants;
+import picasso.parser.tokens.chars.CharToken;
+
+
+/**
+ * Represents the '=' character 
+ * 
+ * @author Therese Elvira Mombou Gatsing
+ */
+
+public class EqualsToken extends CharToken {
+	public EqualsToken() {
+		super(CharConstants.EQUALS);
+	}
+	
+	
+
+}
